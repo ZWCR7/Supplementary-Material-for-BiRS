d7b29cb0be961deb8a9af87fc81a20b7a3a2ec8f
Wei Zhang <76790544+ZWCR7@users.noreply.github.com>
2024-10-15 17:49:52 +0800

    Update Generate_Block_Index.R

--- Application_Code/WGS/Generate_Block_Index.R
+++ Application_Code/WGS/Generate_Block_Index.R
@@ -4,7 +4,7 @@ for (chr in 1:22)
 {
   BP = read_bim(paste0('/mnt/project/Bulk/Previous WGS releases/GATK and GraphTyper WGS/GraphTyper population level WGS variants, PLINK format [200k release]/ukb24305_c', chr, '_b0_v1.bim'))$pos
   
-  Block.seq = unique(seq(1, length(BP), 2000), length(BP))
+  Block.seq = unique(seq(1, length(BP), 5000), length(BP))
   start = Block.seq[-length(Block.seq)]
   end = c(start[-1] - 1, length(BP))
   CHR = rep(chr, length(start))
