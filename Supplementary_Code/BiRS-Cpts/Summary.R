329a8dae4ee8d709764fcdb89c87ff5b58dba969
Wei Zhang <76790544+ZWCR7@users.noreply.github.com>
2024-10-17 19:03:02 +0800

    Update Summary.R

--- Supplementary_Code/BiRS-Cpts/Summary.R
+++ Supplementary_Code/BiRS-Cpts/Summary.R
@@ -5,20 +5,10 @@ TrateBSD = matrix(0, 100, 20)
 FrateBSD = matrix(0, 100, 20)
 DifTiBSD = matrix(0, 100, 20)
 
-powerSBB = rep(100, 20)
-TrateSBB = matrix(0, 100, 20)
-FrateSBB = matrix(0, 100, 20)
-DifTiSBB = matrix(0, 100, 20)
-
 powerWBB = rep(100, 20)
 TrateWBB = matrix(0, 100, 20)
 FrateWBB = matrix(0, 100, 20)
 DifTiWBB = matrix(0, 100, 20)
-
-powerBSB = rep(100, 20)
-TrateBSB = matrix(0, 100, 20)
-FrateBSB = matrix(0, 100, 20)
-DifTiBSB = matrix(0, 100, 20)
 
 for (i in 1:20)
 { 
@@ -27,9 +17,7 @@ for (i in 1:20)
   nsimu = length(res); p = 8192
   
   EstBSD = matrix(0, nsimu, p)
-  EstSBB = matrix(0, nsimu, p)
   EstWBB = matrix(0, nsimu, p)
-  EstBSB = matrix(0, nsimu, p)
   
   for (j in 1:length(res))
   {
@@ -47,27 +35,10 @@ for (i in 1:20)
       powerBSD[i] = powerBSD[i] - 1
     }
 
-    if (!is.character(res[[j]]$reBSB))
-    {
-      Resj = res[[j]]$reBSB$SBDCF_res
-
-      for (k in 1:nrow(Resj))
-      {
-        EstBSB[j, (Resj$startind[k]:Resj$endind[k])] = 1
-      }
-    }
-    else
-    {
-      powerBSB[i] = powerBSB[i] - 1
-    }
-    
     EstWBB[j, ] = res[[j]]$reWBB
-    EstSBB[j, ] = res[[j]]$reSBB
     
     DifTiBSD[j, i] = as.numeric(res[[j]]$DifTBSD, units = 'secs')
-    DifTiBSB[j, i] = as.numeric(res[[j]]$DifTBSB, units = 'secs')
     DifTiWBB[j, i] = as.numeric(res[[j]]$DifTWBB, units = 'secs')
-    DifTiSBB[j, i] = as.numeric(res[[j]]$DifTSBB, units = 'secs')
   }
   
   mu = mul
@@ -84,15 +55,7 @@ for (i in 1:20)
       TrateBSD[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
       FrateBSD[l, i] = length(which(Diffl == 3))/length(which(EstBSD[l, ] != 0))
     }
-    
-    if (length(which(EstSBB[l, ] != 0)) != 0)
-    {
-      Diffl = EstSBB[l, ] - true.ind
-      
-      TrateSBB[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
-      FrateSBB[l, i] = length(which(Diffl == 3))/length(which(EstSBB[l, ] != 0))
-    }
-    
+  
     if (length(which(EstWBB[l, ] != 0)) != 0)
     {
       Diffl = EstWBB[l, ] - true.ind
@@ -100,20 +63,10 @@ for (i in 1:20)
       TrateWBB[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
       FrateWBB[l, i] = length(which(Diffl == 3))/length(which(EstWBB[l, ] != 0))
     }
-    
-    if (length(which(EstBSB[l, ] != 0)) != 0)
-    {
-      Diffl = EstBSB[l, ] - true.ind
-
-      TrateBSB[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
-      FrateBSB[l, i] = length(which(Diffl == 3))/length(which(EstBSB[l, ] != 0))
-    }
   }
 }
 
 save(list = c('FrateBSD', 'TrateBSD', 'powerBSD', 'DifTiBSD'), file = 'Review-BiRS-cpts/MES-BSD_Describe.RData')
-save(list = c('FrateBSB', 'TrateBSB', 'powerBSB', 'DifTiBSB'), file = 'Review-BiRS-cpts/MES-BSB_Describe.RData')
-save(list = c('FrateSBB', 'TrateSBB', 'powerSBB', 'DifTiSBB'), file = 'Review-BiRS-cpts/MES-SBB_Describe.RData')
 save(list = c('FrateWBB', 'TrateWBB', 'powerWBB', 'DifTiWBB'), file = 'Review-BiRS-cpts/MES-WBB_Describe.RData')
 ###############################################################################################################################################
 
@@ -124,31 +77,19 @@ TrateBSD = matrix(0, 100, 20)
 FrateBSD = matrix(0, 100, 20)
 DifTiBSD = matrix(0, 100, 20)
 
-powerSBB = rep(100, 20)
-TrateSBB = matrix(0, 100, 20)
-FrateSBB = matrix(0, 100, 20)
-DifTiSBB = matrix(0, 100, 20)
-
 powerWBB = rep(100, 20)
 TrateWBB = matrix(0, 100, 20)
 FrateWBB = matrix(0, 100, 20)
 DifTiWBB = matrix(0, 100, 20)
 
-powerBSB = rep(100, 20)
-TrateBSB = matrix(0, 100, 20)
-FrateBSB = matrix(0, 100, 20)
-DifTiBSB = matrix(0, 100, 20)
-
 for (i in 1:20)
 { 
   load(paste0('Review-BiRS-cpts/MNS-Mulist', i, '.RData'))
-
+  
   nsimu = length(res); p = 8192
   
   EstBSD = matrix(0, nsimu, p)
-  EstSBB = matrix(0, nsimu, p)
   EstWBB = matrix(0, nsimu, p)
-  EstBSB = matrix(0, nsimu, p)
   
   for (j in 1:length(res))
   {
@@ -166,27 +107,10 @@ for (i in 1:20)
       powerBSD[i] = powerBSD[i] - 1
     }
 
-    if (!is.character(res[[j]]$reBSB))
-    {
-      Resj = res[[j]]$reBSB$SBDCF_res
-
-      for (k in 1:nrow(Resj))
-      {
-        EstBSB[j, (Resj$startind[k]:Resj$endind[k])] = 1
-      }
-    }
-    else
-    {
-      powerBSB[i] = powerBSB[i] - 1
-    }
-    
     EstWBB[j, ] = res[[j]]$reWBB
-    EstSBB[j, ] = res[[j]]$reSBB
     
     DifTiBSD[j, i] = as.numeric(res[[j]]$DifTBSD, units = 'secs')
-    DifTiBSB[j, i] = as.numeric(res[[j]]$DifTBSB, units = 'secs')
     DifTiWBB[j, i] = as.numeric(res[[j]]$DifTWBB, units = 'secs')
-    DifTiSBB[j, i] = as.numeric(res[[j]]$DifTSBB, units = 'secs')
   }
   
   mu = mul
@@ -203,15 +127,7 @@ for (i in 1:20)
       TrateBSD[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
       FrateBSD[l, i] = length(which(Diffl == 3))/length(which(EstBSD[l, ] != 0))
     }
-    
-    if (length(which(EstSBB[l, ] != 0)) != 0)
-    {
-      Diffl = EstSBB[l, ] - true.ind
-      
-      TrateSBB[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
-      FrateSBB[l, i] = length(which(Diffl == 3))/length(which(EstSBB[l, ] != 0))
-    }
-    
+  
     if (length(which(EstWBB[l, ] != 0)) != 0)
     {
       Diffl = EstWBB[l, ] - true.ind
@@ -219,20 +135,10 @@ for (i in 1:20)
       TrateWBB[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
       FrateWBB[l, i] = length(which(Diffl == 3))/length(which(EstWBB[l, ] != 0))
     }
-    
-    if (length(which(EstBSB[l, ] != 0)) != 0)
-    {
-      Diffl = EstBSB[l, ] - true.ind
-
-      TrateBSB[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
-      FrateBSB[l, i] = length(which(Diffl == 3))/length(which(EstBSB[l, ] != 0))
-    }
   }
 }
 
 save(list = c('FrateBSD', 'TrateBSD', 'powerBSD', 'DifTiBSD'), file = 'Review-BiRS-cpts/MNS-BSD_Describe.RData')
-save(list = c('FrateBSB', 'TrateBSB', 'powerBSB', 'DifTiBSB'), file = 'Review-BiRS-cpts/MNS-BSB_Describe.RData')
-save(list = c('FrateSBB', 'TrateSBB', 'powerSBB', 'DifTiSBB'), file = 'Review-BiRS-cpts/MNS-SBB_Describe.RData')
 save(list = c('FrateWBB', 'TrateWBB', 'powerWBB', 'DifTiWBB'), file = 'Review-BiRS-cpts/MNS-WBB_Describe.RData')
 ###############################################################################################################################################
 rm(list = ls())
@@ -242,32 +148,19 @@ TrateBSD = matrix(0, 100, 20)
 FrateBSD = matrix(0, 100, 20)
 DifTiBSD = matrix(0, 100, 20)
 
-powerSBB = rep(100, 20)
-TrateSBB = matrix(0, 100, 20)
-FrateSBB = matrix(0, 100, 20)
-DifTiSBB = matrix(0, 100, 20)
-
 powerWBB = rep(100, 20)
 TrateWBB = matrix(0, 100, 20)
 FrateWBB = matrix(0, 100, 20)
 DifTiWBB = matrix(0, 100, 20)
 
-powerBSB = rep(100, 20)
-TrateBSB = matrix(0, 100, 20)
-FrateBSB = matrix(0, 100, 20)
-DifTiBSB = matrix(0, 100, 20)
-
 for (i in 1:20)
 { 
   load(paste0('Review-BiRS-cpts/WES-Mulist', i, '.RData'))
   
-  #mu = mul
   nsimu = length(res); p = 8192
   
   EstBSD = matrix(0, nsimu, p)
-  EstSBB = matrix(0, nsimu, p)
   EstWBB = matrix(0, nsimu, p)
-  EstBSB = matrix(0, nsimu, p)
   
   for (j in 1:length(res))
   {
@@ -285,30 +178,13 @@ for (i in 1:20)
       powerBSD[i] = powerBSD[i] - 1
     }
 
-    if (!is.character(res[[j]]$reBSB))
-    {
-      Resj = res[[j]]$reBSB$SBDCF_res
-
-      for (k in 1:nrow(Resj))
-      {
-        EstBSB[j, (Resj$startind[k]:Resj$endind[k])] = 1
-      }
-    }
-    else
-    {
-      powerBSB[i] = powerBSB[i] - 1
-    }
-    
     EstWBB[j, ] = res[[j]]$reWBB
-    EstSBB[j, ] = res[[j]]$reSBB
     
     DifTiBSD[j, i] = as.numeric(res[[j]]$DifTBSD, units = 'secs')
-    DifTiBSB[j, i] = as.numeric(res[[j]]$DifTBSB, units = 'secs')
     DifTiWBB[j, i] = as.numeric(res[[j]]$DifTWBB, units = 'secs')
-    DifTiSBB[j, i] = as.numeric(res[[j]]$DifTSBB, units = 'secs')
   }
   
-  #mu = mul
+  mu = mul
   true.ind = rep(0, p)
   true.ind[which(mu != 0)] = 1
   true.ind[which(mu == 0)] = -2
@@ -322,15 +198,7 @@ for (i in 1:20)
       TrateBSD[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
       FrateBSD[l, i] = length(which(Diffl == 3))/length(which(EstBSD[l, ] != 0))
     }
-    
-    if (length(which(EstSBB[l, ] != 0)) != 0)
-    {
-      Diffl = EstSBB[l, ] - true.ind
-      
-      TrateSBB[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
-      FrateSBB[l, i] = length(which(Diffl == 3))/length(which(EstSBB[l, ] != 0))
-    }
-    
+  
     if (length(which(EstWBB[l, ] != 0)) != 0)
     {
       Diffl = EstWBB[l, ] - true.ind
@@ -338,20 +206,10 @@ for (i in 1:20)
       TrateWBB[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
       FrateWBB[l, i] = length(which(Diffl == 3))/length(which(EstWBB[l, ] != 0))
     }
-    
-    if (length(which(EstBSB[l, ] != 0)) != 0)
-    {
-      Diffl = EstBSB[l, ] - true.ind
-
-      TrateBSB[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
-      FrateBSB[l, i] = length(which(Diffl == 3))/length(which(EstBSB[l, ] != 0))
-    }
   }
 }
 
 save(list = c('FrateBSD', 'TrateBSD', 'powerBSD', 'DifTiBSD'), file = 'Review-BiRS-cpts/WES-BSD_Describe.RData')
-save(list = c('FrateBSB', 'TrateBSB', 'powerBSB', 'DifTiBSB'), file = 'Review-BiRS-cpts/WES-BSB_Describe.RData')
-save(list = c('FrateSBB', 'TrateSBB', 'powerSBB', 'DifTiSBB'), file = 'Review-BiRS-cpts/WES-SBB_Describe.RData')
 save(list = c('FrateWBB', 'TrateWBB', 'powerWBB', 'DifTiWBB'), file = 'Review-BiRS-cpts/WES-WBB_Describe.RData')
 ###############################################################################################################################################
 
@@ -362,39 +220,26 @@ TrateBSD = matrix(0, 100, 20)
 FrateBSD = matrix(0, 100, 20)
 DifTiBSD = matrix(0, 100, 20)
 
-powerSBB = rep(100, 20)
-TrateSBB = matrix(0, 100, 20)
-FrateSBB = matrix(0, 100, 20)
-DifTiSBB = matrix(0, 100, 20)
-
 powerWBB = rep(100, 20)
 TrateWBB = matrix(0, 100, 20)
 FrateWBB = matrix(0, 100, 20)
 DifTiWBB = matrix(0, 100, 20)
 
-powerBSB = rep(100, 20)
-TrateBSB = matrix(0, 100, 20)
-FrateBSB = matrix(0, 100, 20)
-DifTiBSB = matrix(0, 100, 20)
-
 for (i in 1:20)
 { 
   load(paste0('Review-BiRS-cpts/WNS-Mulist', i, '.RData'))
   
-  #mu = mul
   nsimu = length(res); p = 8192
   
   EstBSD = matrix(0, nsimu, p)
-  EstSBB = matrix(0, nsimu, p)
   EstWBB = matrix(0, nsimu, p)
-  EstBSB = matrix(0, nsimu, p)
   
   for (j in 1:length(res))
   {
     if (!is.character(res[[j]]$reBSD))
     {
       Resj = res[[j]]$reBSD$BSDCF_res
-
+      
       for (k in 1:nrow(Resj))
       {
         EstBSD[j, (Resj$startind[k]:Resj$endind[k])] = 1
@@ -405,30 +250,13 @@ for (i in 1:20)
       powerBSD[i] = powerBSD[i] - 1
     }
 
-    if (!is.character(res[[j]]$reBSB))
-    {
-      Resj = res[[j]]$reBSB$SBDCF_res
-
-      for (k in 1:nrow(Resj))
-      {
-        EstBSB[j, (Resj$startind[k]:Resj$endind[k])] = 1
-      }
-    }
-    else
-    {
-      powerBSB[i] = powerBSB[i] - 1
-    }
-    
     EstWBB[j, ] = res[[j]]$reWBB
-    EstSBB[j, ] = res[[j]]$reSBB
     
     DifTiBSD[j, i] = as.numeric(res[[j]]$DifTBSD, units = 'secs')
-    DifTiBSB[j, i] = as.numeric(res[[j]]$DifTBSB, units = 'secs')
     DifTiWBB[j, i] = as.numeric(res[[j]]$DifTWBB, units = 'secs')
-    DifTiSBB[j, i] = as.numeric(res[[j]]$DifTSBB, units = 'secs')
   }
   
-  #mu = mul
+  mu = mul
   true.ind = rep(0, p)
   true.ind[which(mu != 0)] = 1
   true.ind[which(mu == 0)] = -2
@@ -438,19 +266,11 @@ for (i in 1:20)
     if (length(which(EstBSD[l, ] != 0)) != 0)
     {
       Diffl = EstBSD[l, ] - true.ind
-
+      
       TrateBSD[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
       FrateBSD[l, i] = length(which(Diffl == 3))/length(which(EstBSD[l, ] != 0))
     }
-
-    if (length(which(EstSBB[l, ] != 0)) != 0)
-    {
-      Diffl = EstSBB[l, ] - true.ind
-      
-      TrateSBB[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
-      FrateSBB[l, i] = length(which(Diffl == 3))/length(which(EstSBB[l, ] != 0))
-    }
-    
+  
     if (length(which(EstWBB[l, ] != 0)) != 0)
     {
       Diffl = EstWBB[l, ] - true.ind
@@ -458,18 +278,8 @@ for (i in 1:20)
       TrateWBB[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
       FrateWBB[l, i] = length(which(Diffl == 3))/length(which(EstWBB[l, ] != 0))
     }
-    
-    if (length(which(EstBSB[l, ] != 0)) != 0)
-    {
-      Diffl = EstBSB[l, ] - true.ind
-
-      TrateBSB[l, i] = length(which(Diffl == 0))/length(which(mu != 0))
-      FrateBSB[l, i] = length(which(Diffl == 3))/length(which(EstBSB[l, ] != 0))
-    }
   }
 }
 
 save(list = c('FrateBSD', 'TrateBSD', 'powerBSD', 'DifTiBSD'), file = 'Review-BiRS-cpts/WNS-BSD_Describe.RData')
-save(list = c('FrateBSB', 'TrateBSB', 'powerBSB', 'DifTiBSB'), file = 'Review-BiRS-cpts/WNS-BSB_Describe.RData')
-save(list = c('FrateSBB', 'TrateSBB', 'powerSBB', 'DifTiSBB'), file = 'Review-BiRS-cpts/WNS-SBB_Describe.RData')
 save(list = c('FrateWBB', 'TrateWBB', 'powerWBB', 'DifTiWBB'), file = 'Review-BiRS-cpts/WNS-WBB_Describe.RData')
