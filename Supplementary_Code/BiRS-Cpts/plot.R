5736c1ca111524ad7414c38e8652a4017ddcd81b
Wei Zhang <76790544+ZWCR7@users.noreply.github.com>
2024-10-17 19:07:33 +0800

    Update plot.R

--- Supplementary_Code/BiRS-Cpts/plot.R
+++ Supplementary_Code/BiRS-Cpts/plot.R
@@ -1,29 +1,20 @@
 rm(list = ls())
 
 load('Review-BiRS-cpts/MES-BSD_Describe.RData')
-#load('Review-BiRS-cpts/MES-BSB_Describe.RData')
 load('Review-BiRS-cpts/MES-WBB_Describe.RData')
-load('Review-BiRS-cpts/MES-SBB_Describe.RData')
 
 powerBSD = matrix(powerBSD/100, 5, 4)
-#powerBSB = matrix(powerBSB/100, 5, 4)
 powerWBB = matrix(powerWBB/100, 5, 4)
-powerSBB = matrix(powerSBB/100, 5, 4)
 
 TRBSD = matrix(apply(TrateBSD, 2, mean), 5, 4)
-#TRBSB = matrix(apply(TrateBSB, 2, mean), 5, 4)
 TRWBB = matrix(apply(TrateWBB, 2, mean), 5, 4)
-TRSBB = matrix(apply(TrateSBB, 2, mean), 5, 4)
 
 FRBSD = matrix(apply(FrateBSD, 2, mean), 5, 4)
-#FRBSB = matrix(apply(FrateBSB, 2, mean), 5, 4)
 FRWBB = matrix(apply(FrateWBB, 2, mean), 5, 4)
-FRSBB = matrix(apply(FrateSBB, 2, mean), 5, 4)
 
 DTBSD = matrix(apply(DifTiBSD, 2, mean), 5, 4)
-#DTBSB = matrix(apply(DifTiBSB, 2, mean), 5, 4)
 DTWBB = matrix(apply(DifTiWBB, 2, mean), 5, 4)
-DTSBB = matrix(apply(DifTiSBB, 2, mean), 5, 4)
+
 
 delta = c(0.20, 0.25, 0.30, 0.35, 0.40)
 for (j in 1:4)
@@ -31,26 +22,21 @@ for (j in 1:4)
   nameTj = paste0('Figures/MES-TPR_beta', j, '.jpg')
   nameFj = paste0('Figures/MES-FDR_beta', j, '.jpg')
   
-  # nameCTj = paste0('Review-Figures/MES-CP-TPR_beta', j, '.jpg')
-  # nameCFj = paste0('Review-Figures/MES-CP-FDR_beta', j, '.jpg')
-  
   mainTj = paste0('MES, beta = ', j, ' TPR')
   mainFj = paste0('MES, beta = ', j, ' FDR')
   
   jpeg(filename = nameTj, width = 1200, height = 900, quality = 100)
   plot(delta, TRBSD[ ,j], type = 'b', pch = 1, lty = 1, lwd = 4, col = 1, main = mainTj, 
        ylab = "", xlab = "delta", cex.lab = 3, cex.axis = 2.8, ylim = c(0, 1), cex.main = 4)
-  lines(delta, TRSBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
-  lines(delta, TRWBB[ ,j], type = "b", pch = 3, lty = 1, col = 3, lwd = 4)
-  legend("right", c("BSD", "SBB", "WBB"), pch = c(1, 2, 3), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2, 3), lwd = 4)
+  lines(delta, TRWBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
+  legend("right", c("BSD", "WBB"), pch = c(1, 2), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2), lwd = 4)
   dev.off()
   
   jpeg(filename = nameFj, width = 1200, height = 900, quality = 100)
   plot(delta, FRBSD[ ,j], type = 'b', pch = 1, lty = 1, lwd = 4, col = 1, main = mainFj, 
        ylab = "", xlab = "delta", cex.lab = 3, cex.axis = 2.8, ylim = c(0, 0.5), cex.main = 4)
-  lines(delta, FRSBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
-  lines(delta, FRWBB[ ,j], type = "b", pch = 3, lty = 1, col = 3, lwd = 4)
-  legend("topright", c("BSD", "SBB", "WBB"), pch = c(1, 2, 3), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2, 3), lwd = 4)
+  lines(delta, FRWBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
+  legend("topright", c("BSD", "WBB"), pch = c(1, 2), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2), lwd = 4)
   dev.off()
 }
 
@@ -59,24 +45,20 @@ for (j in 1:4)
 rm(list = ls())
 
 load('Review-BiRS-cpts/MNS-BSD_Describe.RData')
-#load('Review-BiRS-cpts/MNS-BSB_Describe.RData')
 load('Review-BiRS-cpts/MNS-WBB_Describe.RData')
-load('Review-BiRS-cpts/MNS-SBB_Describe.RData')
 
 powerBSD = matrix(powerBSD/100, 5, 4)
-#powerBSB = matrix(powerBSB/100, 5, 4)
 powerWBB = matrix(powerWBB/100, 5, 4)
-powerSBB = matrix(powerSBB/100, 5, 4)
 
 TRBSD = matrix(apply(TrateBSD, 2, mean), 5, 4)
-#TRBSB = matrix(apply(TrateBSB, 2, mean), 5, 4)
 TRWBB = matrix(apply(TrateWBB, 2, mean), 5, 4)
-TRSBB = matrix(apply(TrateSBB, 2, mean), 5, 4)
 
 FRBSD = matrix(apply(FrateBSD, 2, mean), 5, 4)
-#FRBSB = matrix(apply(FrateBSB, 2, mean), 5, 4)
 FRWBB = matrix(apply(FrateWBB, 2, mean), 5, 4)
-FRSBB = matrix(apply(FrateSBB, 2, mean), 5, 4)
+
+DTBSD = matrix(apply(DifTiBSD, 2, mean), 5, 4)
+DTWBB = matrix(apply(DifTiWBB, 2, mean), 5, 4)
+
 
 delta = c(0.20, 0.25, 0.30, 0.35, 0.40)
 for (j in 1:4)
@@ -84,26 +66,21 @@ for (j in 1:4)
   nameTj = paste0('Figures/MNS-TPR_beta', j, '.jpg')
   nameFj = paste0('Figures/MNS-FDR_beta', j, '.jpg')
   
-  # nameCTj = paste0('Review-Figures/MNS-CP-TPR_beta', j, '.jpg')
-  # nameCFj = paste0('Review-Figures/MNS-CP-FDR_beta', j, '.jpg')
-  
   mainTj = paste0('MNS, beta = ', j, ' TPR')
   mainFj = paste0('MNS, beta = ', j, ' FDR')
   
   jpeg(filename = nameTj, width = 1200, height = 900, quality = 100)
   plot(delta, TRBSD[ ,j], type = 'b', pch = 1, lty = 1, lwd = 4, col = 1, main = mainTj, 
        ylab = "", xlab = "delta", cex.lab = 3, cex.axis = 2.8, ylim = c(0, 1), cex.main = 4)
-  lines(delta, TRSBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
-  lines(delta, TRWBB[ ,j], type = "b", pch = 3, lty = 1, col = 3, lwd = 4)
-  legend("right", c("BSD", "SBB", "WBB"), pch = c(1, 2, 3), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2, 3), lwd = 4)
+  lines(delta, TRWBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
+  legend("right", c("BSD", "WBB"), pch = c(1, 2), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2), lwd = 4)
   dev.off()
   
   jpeg(filename = nameFj, width = 1200, height = 900, quality = 100)
   plot(delta, FRBSD[ ,j], type = 'b', pch = 1, lty = 1, lwd = 4, col = 1, main = mainFj, 
        ylab = "", xlab = "delta", cex.lab = 3, cex.axis = 2.8, ylim = c(0, 0.5), cex.main = 4)
-  lines(delta, FRSBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
-  lines(delta, FRWBB[ ,j], type = "b", pch = 3, lty = 1, col = 3, lwd = 4)
-  legend("topright", c("BSD", "SBB", "WBB"), pch = c(1, 2, 3), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2, 3), lwd = 4)
+  lines(delta, FRWBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
+  legend("topright", c("BSD", "WBB"), pch = c(1, 2), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2), lwd = 4)
   dev.off()
 }
 
@@ -112,24 +89,19 @@ for (j in 1:4)
 rm(list = ls())
 
 load('Review-BiRS-cpts/WES-BSD_Describe.RData')
-#load('Review-BiRS-cpts/WES-BSB_Describe.RData')
 load('Review-BiRS-cpts/WES-WBB_Describe.RData')
-load('Review-BiRS-cpts/WES-SBB_Describe.RData')
 
 powerBSD = matrix(powerBSD/100, 5, 4)
-#powerBSB = matrix(powerBSB/100, 5, 4)
 powerWBB = matrix(powerWBB/100, 5, 4)
-powerSBB = matrix(powerSBB/100, 5, 4)
 
 TRBSD = matrix(apply(TrateBSD, 2, mean), 5, 4)
-#TRBSB = matrix(apply(TrateBSB, 2, mean), 5, 4)
 TRWBB = matrix(apply(TrateWBB, 2, mean), 5, 4)
-TRSBB = matrix(apply(TrateSBB, 2, mean), 5, 4)
 
 FRBSD = matrix(apply(FrateBSD, 2, mean), 5, 4)
-#FRBSB = matrix(apply(FrateBSB, 2, mean), 5, 4)
 FRWBB = matrix(apply(FrateWBB, 2, mean), 5, 4)
-FRSBB = matrix(apply(FrateSBB, 2, mean), 5, 4)
+
+DTBSD = matrix(apply(DifTiBSD, 2, mean), 5, 4)
+DTWBB = matrix(apply(DifTiWBB, 2, mean), 5, 4)
 
 
 delta = c(0.20, 0.25, 0.30, 0.35, 0.40)
@@ -138,26 +110,21 @@ for (j in 1:4)
   nameTj = paste0('Figures/WES-TPR_beta', j, '.jpg')
   nameFj = paste0('Figures/WES-FDR_beta', j, '.jpg')
   
-  # nameCTj = paste0('Review-Figures/WES-CP-TPR_beta', j, '.jpg')
-  # nameCFj = paste0('Review-Figures/WES-CP-FDR_beta', j, '.jpg')
-  
   mainTj = paste0('WES, beta = ', j, ' TPR')
   mainFj = paste0('WES, beta = ', j, ' FDR')
   
   jpeg(filename = nameTj, width = 1200, height = 900, quality = 100)
   plot(delta, TRBSD[ ,j], type = 'b', pch = 1, lty = 1, lwd = 4, col = 1, main = mainTj, 
        ylab = "", xlab = "delta", cex.lab = 3, cex.axis = 2.8, ylim = c(0, 1), cex.main = 4)
-  lines(delta, TRSBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
-  lines(delta, TRWBB[ ,j], type = "b", pch = 3, lty = 1, col = 3, lwd = 4)
-  legend("right", c("BSD", "SBB", "WBB"), pch = c(1, 2, 3), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2, 3), lwd = 4)
+  lines(delta, TRWBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
+  legend("right", c("BSD", "WBB"), pch = c(1, 2), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2), lwd = 4)
   dev.off()
   
   jpeg(filename = nameFj, width = 1200, height = 900, quality = 100)
   plot(delta, FRBSD[ ,j], type = 'b', pch = 1, lty = 1, lwd = 4, col = 1, main = mainFj, 
        ylab = "", xlab = "delta", cex.lab = 3, cex.axis = 2.8, ylim = c(0, 0.5), cex.main = 4)
-  lines(delta, FRSBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
-  lines(delta, FRWBB[ ,j], type = "b", pch = 3, lty = 1, col = 3, lwd = 4)
-  legend("topright", c("BSD", "SBB", "WBB"), pch = c(1, 2, 3), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2, 3), lwd = 4)
+  lines(delta, FRWBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
+  legend("topright", c("BSD", "WBB"), pch = c(1, 2), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2), lwd = 4)
   dev.off()
 }
 
@@ -166,29 +133,20 @@ for (j in 1:4)
 rm(list = ls())
 
 load('Review-BiRS-cpts/WNS-BSD_Describe.RData')
-#load('Review-BiRS-cpts/WNS-BSB_Describe.RData')
 load('Review-BiRS-cpts/WNS-WBB_Describe.RData')
-load('Review-BiRS-cpts/WNS-SBB_Describe.RData')
 
 powerBSD = matrix(powerBSD/100, 5, 4)
-#powerBSB = matrix(powerBSB/100, 5, 4)
 powerWBB = matrix(powerWBB/100, 5, 4)
-powerSBB = matrix(powerSBB/100, 5, 4)
 
 TRBSD = matrix(apply(TrateBSD, 2, mean), 5, 4)
-#TRBSB = matrix(apply(TrateBSB, 2, mean), 5, 4)
 TRWBB = matrix(apply(TrateWBB, 2, mean), 5, 4)
-TRSBB = matrix(apply(TrateSBB, 2, mean), 5, 4)
 
 FRBSD = matrix(apply(FrateBSD, 2, mean), 5, 4)
-#FRBSB = matrix(apply(FrateBSB, 2, mean), 5, 4)
 FRWBB = matrix(apply(FrateWBB, 2, mean), 5, 4)
-FRSBB = matrix(apply(FrateSBB, 2, mean), 5, 4)
 
 DTBSD = matrix(apply(DifTiBSD, 2, mean), 5, 4)
-#DTBSB = matrix(apply(DifTiBSB, 2, mean), 5, 4)
 DTWBB = matrix(apply(DifTiWBB, 2, mean), 5, 4)
-DTSBB = matrix(apply(DifTiSBB, 2, mean), 5, 4)
+
 
 delta = c(0.20, 0.25, 0.30, 0.35, 0.40)
 for (j in 1:4)
@@ -196,26 +154,21 @@ for (j in 1:4)
   nameTj = paste0('Figures/WNS-TPR_beta', j, '.jpg')
   nameFj = paste0('Figures/WNS-FDR_beta', j, '.jpg')
   
-  # nameCTj = paste0('Review-Figures/WNS-CP-TPR_beta', j, '.jpg')
-  # nameCFj = paste0('Review-Figures/WNS-CP-FDR_beta', j, '.jpg')
-  
   mainTj = paste0('WNS, beta = ', j, ' TPR')
   mainFj = paste0('WNS, beta = ', j, ' FDR')
   
   jpeg(filename = nameTj, width = 1200, height = 900, quality = 100)
   plot(delta, TRBSD[ ,j], type = 'b', pch = 1, lty = 1, lwd = 4, col = 1, main = mainTj, 
        ylab = "", xlab = "delta", cex.lab = 3, cex.axis = 2.8, ylim = c(0, 1), cex.main = 4)
-  lines(delta, TRSBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
-  lines(delta, TRWBB[ ,j], type = "b", pch = 3, lty = 1, col = 3, lwd = 4)
-  legend("right", c("BSD", "SBB", "WBB"), pch = c(1, 2, 3), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2, 3), lwd = 4)
+  lines(delta, TRWBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
+  legend("right", c("BSD", "WBB"), pch = c(1, 2), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2), lwd = 4)
   dev.off()
   
   jpeg(filename = nameFj, width = 1200, height = 900, quality = 100)
   plot(delta, FRBSD[ ,j], type = 'b', pch = 1, lty = 1, lwd = 4, col = 1, main = mainFj, 
        ylab = "", xlab = "delta", cex.lab = 3, cex.axis = 2.8, ylim = c(0, 0.5), cex.main = 4)
-  lines(delta, FRSBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
-  lines(delta, FRWBB[ ,j], type = "b", pch = 3, lty = 1, col = 3, lwd = 4)
-  legend("topright", c("BSD", "SBB", "WBB"), pch = c(1, 2, 3), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2, 3), lwd = 4)
+  lines(delta, FRWBB[ ,j], type = "b", pch = 2, lty = 1, col = 2, lwd = 4)
+  legend("topright", c("BSD", "WBB"), pch = c(1, 2), lty = 1, box.col = "grey", cex = 3.5, col = c(1, 2), lwd = 4)
   dev.off()
 }
 
